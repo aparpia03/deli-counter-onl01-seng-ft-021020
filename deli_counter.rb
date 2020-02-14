d3d2f77ecc23f343231326378de31050fb827ba4
Abbas Parpia <aparpia03@gmail.com>
2020-02-14 00:36:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,11 +3,4 @@ katz_deli = []
 
 def line(katz_deli)
    queue_in_line == []
-    puts "The line is currently empty."
-  else
-  queue_in_line.each do |queue|
-    katz_deli.push(queue)
-    end
-  end
-end
-
+   if katz_deli

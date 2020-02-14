814ffdd4cc2503343e4967d697b49e5f1a2da0fb
Abbas Parpia <aparpia03@gmail.com>
2020-02-14 01:21:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -25,7 +25,7 @@ end
 def now_serving(katz_deli,name)
   if katz_deli.empty?
     puts "There is nobody waiting to be served!"
-  else puts "Currently serving #{katz_deli}"
+  else puts "Currently serving #{katz_deli.first}"
   
 end  
 

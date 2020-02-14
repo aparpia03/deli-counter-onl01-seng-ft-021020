ce76fe42076a0a364e402400b5d4407ccefc7912
Abbas Parpia <aparpia03@gmail.com>
2020-02-14 01:24:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,8 +18,8 @@ end
 end
 
 def take_a_number(katz_deli, name)
-  katz_deli << name 
-  puts "Welcome #{name}." "You are #{katz_deli.length} in line."
+  katz_deli.push(name)
+  puts "Welcome #{name}. You are #{katz_deli.length} in line."
 end  
 
 def now_serving(katz_deli)
